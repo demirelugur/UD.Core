3439386ae381fad7edf1dd5c0631fd2284349f2c
Uğur DEMİREL <ugur.demirel6@saglik.gov.tr>
2026-03-09 11:03:26 +0300

    basit düzenleme

---
+++ UD.Core/Attributes/DefaultValue/UDDefaultMacAddressAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace UD.Core.Attributes.DefaultValue
+{
+    using System.ComponentModel;
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class UDDefaultMacAddressAttribute : DefaultValueAttribute
+    {
+        public UDDefaultMacAddressAttribute() : base(typeof(string), "00:00:00:00:00:00") { }
+    }
+}
--- UD.Core/Attributes/DefaultValue/UDDefaultMacAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UD.Core.Attributes.DefaultValue
-{
-    using System.ComponentModel;
-
-    public sealed class UDDefaultMacAttribute : DefaultValueAttribute
-    {
-        public UDDefaultMacAttribute() : base(typeof(string), "00:00:00:00:00:00") { }
-    }
-}
