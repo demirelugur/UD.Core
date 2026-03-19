207a9393a0f74e669427905e06a822e3663bd438
Uğur Demirel <ugur.demirel@outlook.com>
2026-03-19 06:25:38 +0300

    basit düzenleme

--- UD.Core/Extensions/OtherExtensions.cs
+++ UD.Core/Extensions/OtherExtensions.cs
@@ -250,19 +250,19 @@
             }
             catch { return ""; }
         }
-        /// <summary>Verilen assembly içerisinde bulunan ve <paramref name="typeInterface"/> arayüzünü uygulayan veya <paramref name="typeBaseClass"/> sınıfından türeyen tüm repository sınıflarını otomatik olarak tarar ve bağımlılık enjeksiyonuna Scoped yaşam süresi ile ekler. Bu sayede her repository için manuel olarak AddScoped tanımı yapmaya gerek kalmaz.</summary>
+        /// <summary>Verilen assembly içerisinde bulunan ve <paramref name="typeInterface"/> arayüzünü uygulayan veya <paramref name="typeBaseclass"/> sınıfından türeyen tüm repository sınıflarını otomatik olarak tarar ve bağımlılık enjeksiyonuna Scoped yaşam süresi ile ekler. Bu sayede her repository için manuel olarak AddScoped tanımı yapmaya gerek kalmaz.</summary>
         /// <param name="services">Bağımlılık enjeksiyon konteyneri</param>
         /// <param name="assembly">Repository sınıflarının bulunduğu assembly</param>
         /// <param name="typeInterface">Repository arayüzünün tipi (örneğin, typeof(IRepository&lt;&gt;))</param>
-        /// <param name="typeBaseClass">Repository> sınıflarının türediği temel sınıfın tipi (örneğin, typeof(BaseRepository&lt;&gt;))</param>
+        /// <param name="typeBaseclass">Repository> sınıflarının türediği temel sınıfın tipi (örneğin, typeof(BaseRepository&lt;&gt;))</param>
         /// <returns>Güncellenmiş IServiceCollection nesnesi</returns>
-        public static IServiceCollection AddScopedRange(this IServiceCollection services, Assembly assembly, Type typeInterface, Type typeBaseClass)
+        public static IServiceCollection AddScopedRange(this IServiceCollection services, Assembly assembly, Type typeInterface, Type typeBaseclass)
         {
             Guard.ThrowIfNull(services, nameof(services));
             Guard.ThrowIfNull(assembly, nameof(assembly));
             Guard.ThrowIfNull(typeInterface, nameof(typeInterface));
-            Guard.ThrowIfNull(typeBaseClass, nameof(typeBaseClass));
-            var types = assembly.GetTypes().Where(x => !x.IsAbstract && !x.IsInterface && x.IsSubclassOfOpenGeneric(typeBaseClass)).ToArray();
+            Guard.ThrowIfNull(typeBaseclass, nameof(typeBaseclass));
+            var types = assembly.GetTypes().Where(x => !x.IsAbstract && !x.IsInterface && x.IsSubclassOfOpenGeneric(typeBaseclass)).ToArray();
             foreach (var implementation in types)
             {
                 var interfaces = implementation.GetInterfaces().Where(x => x.IsImplementsOpenGenericInterface(typeInterface)).ToArray();
