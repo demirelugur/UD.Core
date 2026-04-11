27a84f421db1b2f63051b33db392947f605100da
Uğur Demirel <ugur.demirel@outlook.com>
2026-04-11 12:50:56 +0300

    BaseReadOnlyService iptal edildi...

--- UD.Core/Helper/Services/BaseService.cs
+++ UD.Core/Helper/Services/BaseService.cs
@@ -1,6 +1,7 @@
 ﻿namespace UD.Core.Helper.Services
 {
     using AutoMapper;
+    using AutoMapper.QueryableExtensions;
     using Microsoft.EntityFrameworkCore;
     using System.Linq.Dynamic.Core;
     using System.Linq.Expressions;
@@ -8,18 +9,21 @@
     using UD.Core.Helper.Configuration;
     using UD.Core.Helper.Paging;
     using UD.Core.Helper.Validation;
-    public interface IBaseService<TContext, TEntity, TEntityDto, TEntityListDto, TSearchDto> : IBaseReadOnlyService<TContext, TEntity, TEntityDto, TEntityListDto, TSearchDto>
+    public interface IBaseService<TContext, TEntity, TEntityDto, TEntityListDto, TSearchDto> : IBaseInfrastructureService<TContext, TEntity>
     where TContext : DbContext
     where TEntity : class, IBaseEntity
     where TEntityDto : IEntityDto
     where TEntityListDto : IEntityDto
     where TSearchDto : ISearchAndPaginateDto
     {
+        Task<TEntityDto?> GetBySearch(TSearchDto searchDto, CancellationToken cancellationToken = default);
+        Task<TEntityListDto[]> GetAll(TSearchDto searchDto, CancellationToken cancellationToken = default);
+        Task<Paginate<TEntityListDto>> GetAllPaginate(TSearchDto searchDto, bool loadInfo = true, CancellationToken cancellationToken = default);
         Task Delete(TEntity entity, bool autoSave = false, CancellationToken cancellationToken = default);
         Task DeleteByPredicate(Expression<Func<TEntity, bool>> predicate, bool autoSave = false, CancellationToken cancellationToken = default);
         Task DeleteRange(IEnumerable<TEntity> entities, bool autoSave = false, CancellationToken cancellationToken = default);
     }
-    public abstract class BaseService<TContext, TEntity, TEntityDto, TEntityListDto, TSearchDto> : BaseReadOnlyService<TContext, TEntity, TEntityDto, TEntityListDto, TSearchDto>, IBaseReadOnlyService<TContext, TEntity, TEntityDto, TEntityListDto, TSearchDto>
+    public abstract class BaseService<TContext, TEntity, TEntityDto, TEntityListDto, TSearchDto> : BaseInfrastructureService<TContext, TEntity>, IBaseService<TContext, TEntity, TEntityDto, TEntityListDto, TSearchDto>
     where TContext : DbContext
     where TEntity : class, IBaseEntity
     where TEntityDto : IEntityDto
@@ -27,6 +31,14 @@
     where TSearchDto : ISearchAndPaginateDto
     {
         protected BaseService(TContext Context, IMapper Mapper) : base(Context, Mapper) { }
+        protected abstract IQueryable<TEntity> ApplyFiltering(IQueryable<TEntity> query, TSearchDto searchDto);
+        public virtual Task<TEntityDto?> GetBySearch(TSearchDto searchDto, CancellationToken cancellationToken = default) => this.ApplyFiltering(this.DbSet, searchDto).AsNoTracking().ProjectTo<TEntityDto>(this.Mapper.ConfigurationProvider).FirstOrDefaultAsync(cancellationToken);
+        public virtual async Task<TEntityListDto[]> GetAll(TSearchDto searchDto, CancellationToken cancellationToken = default) => (await this.GetAllPaginate(searchDto, false, cancellationToken)).items;
+        public virtual Task<Paginate<TEntityListDto>> GetAllPaginate(TSearchDto searchDto, bool loadInfo = true, CancellationToken cancellationToken = default)
+        {
+            Guard.ThrowIfNull(searchDto, nameof(searchDto));
+            return searchDto.ToPagedList(this.ApplyFiltering(this.DbSet, searchDto).AsNoTracking().ProjectTo<TEntityListDto>(this.Mapper.ConfigurationProvider), loadInfo, cancellationToken);
+        }
         public virtual async Task Delete(TEntity entity, bool autoSave = false, CancellationToken cancellationToken = default)
         {
             if (entity != null)
